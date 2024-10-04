0411f0a9fc2e90653b90a74aa4e6e44a7e85c128
Esencial360 <esencial360yoga@gmail.com>
2024-10-04 11:22:09 +0200

    added bg

--- src/app/components/news-and-blogs/news-and-blogs.component.ts
+++ src/app/components/news-and-blogs/news-and-blogs.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 interface RetreatFeature {
   title: string;
@@ -10,21 +10,44 @@ interface RetreatFeature {
   templateUrl: './news-and-blogs.component.html',
   styleUrl: './news-and-blogs.component.css'
 })
-export class NewsAndBlogsComponent {
+export class NewsAndBlogsComponent implements OnInit {
 
-  retreatFeatures: RetreatFeature[] = [
-    {
-      title: 'Retreats Tailored to You',
-      description: 'Our retreats are designed to fit your needs, whether you\'re a yoga novice or expert. Find the perfect balance of activity and relaxation, and connect with like-minded individuals.'
-    },
-    {
-      title: 'Diverse Yoga Practices',
-      description: 'Experience a variety of yoga styles to enrich your practice. Our expert instructors guide you in finding the style that speaks to you, from calming Yin to dynamic Ashtanga.'
-    },
-    {
-      title: 'Nature and Nurture',
-      description: 'Connect with the calming power of nature. Our retreats are set in beautiful, peaceful locations that offer a perfect backdrop for mindfulness and growth.'
+  @Input()
+  instructorView!: boolean;
+  retreatFeatures: RetreatFeature[] = []
+
+  ngOnInit() {
+    if (this.instructorView) {
+      this.retreatFeatures = [
+        {
+          title: 'Primero Manda tu informacion',
+          description: 'Our retreats are designed to fit your needs, whether you\'re a yoga novice or expert. Find the perfect balance of activity and relaxation, and connect with like-minded individuals.'
+        },
+        {
+          title: 'Evaluaremos tu solicitud',
+          description: 'Experience a variety of yoga styles to enrich your practice. Our expert instructors guide you in finding the style that speaks to you, from calming Yin to dynamic Ashtanga.'
+        },
+        {
+          title: 'Si todo ok, te contactaremos',
+          description: 'Connect with the calming power of nature. Our retreats are set in beautiful, peaceful locations that offer a perfect backdrop for mindfulness and growth.'
+        }
+      ];
+    } else {
+      this.retreatFeatures = [
+        {
+          title: 'Retreats Tailored to You',
+          description: 'Our retreats are designed to fit your needs, whether you\'re a yoga novice or expert. Find the perfect balance of activity and relaxation, and connect with like-minded individuals.'
+        },
+        {
+          title: 'Diverse Yoga Practices',
+          description: 'Experience a variety of yoga styles to enrich your practice. Our expert instructors guide you in finding the style that speaks to you, from calming Yin to dynamic Ashtanga.'
+        },
+        {
+          title: 'Nature and Nurture',
+          description: 'Connect with the calming power of nature. Our retreats are set in beautiful, peaceful locations that offer a perfect backdrop for mindfulness and growth.'
+        }
+      ];
     }
-  ];
+  }
 
 }
