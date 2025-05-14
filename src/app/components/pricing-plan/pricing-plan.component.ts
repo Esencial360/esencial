1d564a70c80e23f234f95b96648c8b5c731ab96d
pablo-gzz-sal <pablo.gzz.sal@gmail.com>
2025-05-14 23:50:48 +0200

    develop-test: added method to pricing

--- src/app/components/pricing-plan/pricing-plan.component.ts
+++ src/app/components/pricing-plan/pricing-plan.component.ts
@@ -1,21 +1,22 @@
 import { Component } from '@angular/core';
 import { environment } from '../../../environments/environment';
+import { AuthService } from '@auth0/auth0-angular';
 
 interface PricingPlan {
   name: string;
   price: number;
   highlight?: string;
   features: string[];
-  image: string
+  image: string;
 }
 
 @Component({
   selector: 'app-pricing-plan',
   templateUrl: './pricing-plan.component.html',
-  styleUrl: './pricing-plan.component.css'
+  styleUrl: './pricing-plan.component.css',
 })
 export class PricingPlanComponent {
-  pullZone: string = environment.pullZone
+  pullZone: string = environment.pullZone;
   plans: PricingPlan[] = [
     {
       name: 'esencial360',
@@ -26,8 +27,8 @@ export class PricingPlanComponent {
         'Acceso completo de la plataforma.',
         'Asesoría personalizada.',
         'Promoción de estudio o marca personal',
-        'Acceso gratutito a talleres especiales y eventos.'
-      ]
+        'Acceso gratutito a talleres especiales y eventos.',
+      ],
     },
     {
       name: 'esencial',
@@ -37,8 +38,9 @@ export class PricingPlanComponent {
         'Accesso a clases de yoga y meditaciones',
         'Asesoría básica.',
         'Descuente a talleres especiales y eventos.',
-      ]
+      ],
     },
   ];
 
+  constructor(public authService: AuthService) {}
 }
