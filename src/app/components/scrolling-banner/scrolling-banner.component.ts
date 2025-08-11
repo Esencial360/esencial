e03ab43599a8badd851d1746c03e3e565249385c
pablo-gzz-sal <pablo.gzz.sal@gmail.com>
2025-08-11 17:31:16 +0200

    fix scrolling bar

--- src/app/components/scrolling-banner/scrolling-banner.component.ts
+++ src/app/components/scrolling-banner/scrolling-banner.component.ts
@@ -1,19 +1,22 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-scrolling-banner',
   templateUrl: './scrolling-banner.component.html',
-  styleUrl: './scrolling-banner.component.css'
+  styleUrl: './scrolling-banner.component.css',
 })
 export class ScrollingBannerComponent {
+  pullZone = environment.pullZone;
 
   logos = [
-    { src: '../../../assets/images/logo.png', alt: 'Framer' },
-    { src: '../../../assets/images/logo.png', alt: 'Flow the Sun' },
-    { src: '../../../assets/images/logo.png', alt: 'Framer' },
-    { src: '../../../assets/images/logo.png', alt: 'Flow the Sun' },
-    { src: '../../../assets/images/logo.png', alt: 'Framer' },
-    { src: '../../../assets/images/logo.png', alt: 'Flow the Sun' }
+    { src: this.pullZone + '/assets/laLoma.png', alt: 'Framer' },
+    { src: this.pullZone + '/assets/powerYoga.png', alt: 'Flow the Sun' },
+    { src: this.pullZone + '/assets/laLoma.png', alt: 'Framer' },
+    { src: this.pullZone + '/assets/powerYoga.png', alt: 'Flow the Sun' },
+    { src: this.pullZone + '/assets/laLoma.png', alt: 'Framer' },
+    { src: this.pullZone + '/assets/powerYoga.png', alt: 'Flow the Sun' },
+    { src: this.pullZone + '/assets/laLoma.png', alt: 'Framer' },
+    { src: this.pullZone + '/assets/powerYoga.png', alt: 'Flow the Sun' },
   ];
-
 }
